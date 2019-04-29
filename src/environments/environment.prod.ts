b837f59b192a8eeb887fb59ccf28da6eaefb3cca
liciolentimo <lentimo@gmail.com>
2019-04-29 15:31:17 +0300

    Deployment to gh pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAP1SfWdDQZKQcxLt8ZU5uCb5MjoDiKfj4",
+    authDomain: "payments-1fe5e.firebaseapp.com",
+    databaseURL: "https://payments-1fe5e.firebaseio.com",
+    projectId: "payments-1fe5e",
+    storageBucket: "payments-1fe5e.appspot.com",
+    messagingSenderId: "713032516121"
+  }
 };
